73ea52827bd5004582077efad2b1edf91823ded7
Tsukamoto Daisuke <d.tsukamoto@secure-cycle.co.jp>
2023-11-01 11:47:51 +0900

    add prettier

--- .prettier.config.cjs
+++ .prettier.config.cjs
@@ -1,13 +1,12 @@
-module.exports = {
+/** @type {import("prettier").Config} */
+const config = {
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   parser: "typescript",
-  importOrder: [
-    "<THIRD_PARTY_MODULES>",
-    "@/.+$",
-    "^[./]",
-  ],
+  importOrder: ["<THIRD_PARTY_MODULES>", "@/.+$", "^[./]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
 };
+
+module.exports = config;
