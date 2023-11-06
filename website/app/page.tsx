e609db9ac944b644f200fbab7fd3c79358d0be37
Tsukamoto Daisuke <d.tsukamoto@secure-cycle.co.jp>
2023-11-06 11:39:46 +0900

    Introduced Panda CSS.

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,13 +1,78 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { css } from "@/styled-system/css";
+
+const cardStyle = css({
+  padding: "1rem 1.2rem",
+  borderRadius: "var(--border-radius)",
+  background: "rgba(var(--card-rgb), 0)",
+  border: "1px solid rgba(var(--card-border-rgb), 0)",
+  transition: "background 200ms, border 200ms",
+  "& span": {
+    display: "inline-block",
+    transition: "transform 200ms",
+  },
+  "& h2": {
+    fontWeight: 600,
+    marginBottom: "0.7rem",
+  },
+  "& p": {
+    margin: "0",
+    opacity: 0.6,
+    fontSize: "0.9rem",
+    lineHeight: 1.5,
+    maxWidth: "30ch",
+  },
+});
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "6rem",
+        minHeight: "100vh",
+      })}
+    >
+      <div
+        className={css({
+          display: "inherit",
+          justifyContent: "inherit",
+          alignItems: "inherit",
+          fontSize: "0.85rem",
+          maxWidth: "var(--max-width)",
+          width: "100%",
+          zIndex: 2,
+          fontFamily: "var(--font-mono)",
+          "& a": {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "0.5rem",
+          },
+          "& p": {
+            position: "relative",
+            margin: "0",
+            padding: "1rem",
+            backgroundColor: "rgba(var(--callout-rgb), 0.5)",
+            border: "1px solid rgba(var(--callout-border-rgb), 0.3)",
+            borderRadius: "var(--border-radius)",
+          },
+        })}
+      >
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          <code
+            className={css({
+              fontWeight: 700,
+              fontFamily: "var(--font-mono)",
+            })}
+          >
+            app/page.tsx
+          </code>
         </p>
         <div>
           <a
@@ -15,7 +80,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -28,9 +93,39 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
+      <div
+        className={css({
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+          padding: "4rem 0",
+          "&::before": {
+            background: "var(--secondary-glow)",
+            borderRadius: "50%",
+            width: "480px",
+            height: "360px",
+            marginLeft: "-400px",
+          },
+          "&::after": {
+            background: "var(--primary-glow)",
+            width: "240px",
+            height: "180px",
+            zIndex: -1,
+          },
+          "&::before, &::after": {
+            content: "''",
+            left: "50%",
+            position: "absolute",
+            filter: "blur(45px)",
+            transform: "translateZ(0)",
+          },
+        })}
+      >
         <Image
-          className={styles.logo}
+          className={css({
+            position: "relative",
+          })}
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
@@ -39,10 +134,23 @@ export default function Home() {
         />
       </div>
 
-      <div className={styles.grid}>
+      <div
+        className={css({
+          display: "grid",
+          gridTemplateColumns: "repeat(4, minmax(25%, auto))",
+          maxWidth: "100%",
+          width: "var(--max-width)",
+          "@media (max-width: 700px)": {
+            gridTemplateColumns: "1fr",
+            marginBottom: "120px",
+            maxWidth: "320px",
+            textAlign: "center",
+          },
+        })}
+      >
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={cardStyle}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -54,7 +162,7 @@ export default function Home() {
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={cardStyle}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -66,7 +174,7 @@ export default function Home() {
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={cardStyle}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -78,7 +186,7 @@ export default function Home() {
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={cardStyle}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -91,5 +199,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
